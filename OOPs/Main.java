c5983525dc88943acb72822f2ab4a3bb91b0fb9b
Avanish Kumar <ak2729@srmist.edu.in>
2025-09-05 11:54:53 +0530

    Added few more attribute in employee class

--- OOPs/Main.java
+++ OOPs/Main.java
@@ -11,5 +11,19 @@ public class Main {
 
         Employee e1 = new Employee();
 
+        e1.id = 11;
+        e1.ename = "Raja";
+        System.out.println(e1.id);
+        System.out.println(e1.ename);
+        e1.work();
+
+        Employee e2 = new Employee();
+
+        e2.id = 12;
+        e2.ename = "Rani";
+
+        System.out.println(e2.id);
+        System.out.println(e2.ename);
+        e1.work();
     }
 }
