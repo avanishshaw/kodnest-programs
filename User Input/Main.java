6b53631751f13f1ec372648899f693c2010b755e
Avanish Kumar <ak2729@srmist.edu.in>
2025-09-09 22:52:08 +0530

    Adding OOPs

--- User Input/Main.java
+++ User Input/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter byte value: ");
+        byte a = sc.nextByte();
+        System.out.println(a);
+
+        System.out.println("Enter short value: ");
+        short b = sc.nextShort();
+        System.out.println(b);
+
+        System.out.println("Enter int value: ");
+        int c = sc.nextInt();
+        System.out.println(c);
+
+        System.out.println("Enter float value: ");
+        float d = sc.nextByte();
+        System.out.println(d);
+
+        System.out.println("Enter long value: ");
+        long e = sc.nextByte();
+        System.out.println(e);
+
+        System.out.println("Enter boolean value: ");
+        boolean f = sc.nextBoolean();
+        System.out.println(f);
+
+    }
 }
