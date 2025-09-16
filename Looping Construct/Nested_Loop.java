b22248c46be3c81e55f80447fb5e3c1379265311
Avanish Kumar <ak2729@srmist.edu.in>
2025-09-16 14:47:58 +0530

    More again

--- Looping Construct/Nested_Loop.java
+++ Looping Construct/Nested_Loop.java
@@ -1,10 +1,21 @@
 public class Nested_Loop {
     public static void main(String[] args) {
-        for(int i = 1; i <= 3; i++){
-            for(int j = 1; j <= 4; j++){
+//        for(int i = 1; i <= 3; i++){
+//            for(int j = 1; j <= 4; j++){
+//                System.out.println("i " + i + " j " + j);
+//            }
+//            System.out.println();
+//        }
+
+        int i = 1;
+        while(i <= 3){
+            int j = 1;
+            while(j <= 4){
                 System.out.println("i " + i + " j " + j);
+                j++;
             }
+            i++;
+            System.out.println();
         }
-        System.out.println();
     }
 }
